0f6459b6b60421aa11da6d1ad240115171a14b2e
Gabriel Soares <70075435+soaressgabriel@users.noreply.github.com>
2021-06-02 21:53:28 -0300

    Update script to compile ui (Windows)

--- rubem_hydrological_dialog_base_ui.py
+++ rubem_hydrological_dialog_base_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'rubem_hydrological_dialog_base.ui'
+# Form implementation generated from reading ui file '..\rubem_hydrological_dialog_base.ui'
 #
 # Created by: PyQt5 UI code generator 5.11.3
 #
