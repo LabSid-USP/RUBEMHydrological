b0e79b38fc46601351e18fcf8c47af7cae8f5529
Gabriel Soares <70075435+soaressgabriel@users.noreply.github.com>
2021-06-25 10:02:07 -0300

    Update files with license info

--- rubem_hydrological.py
+++ rubem_hydrological.py
@@ -1,4 +1,22 @@
 # coding=utf-8
+# RUBEM Hydrological is a QGIS plugin that assists in setup the RUBEM model:
+# Copyright (C) 2021 LabSid PHA EPUSP
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Contact: rubem.hydrological@labsid.eng.br
+
 """
 /***************************************************************************
  **RUBEM Hydrological
@@ -16,15 +34,18 @@
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation; either version 3 of the License, or     *
  *   any later version.                                                    *
  *                                                                         *
  ***************************************************************************/
 """
 
-__author__ = 'rubem.hydrological@labsid.eng.br'
+__author__ = 'LabSid PHA EPUSP'
+__email__ = "rubem.hydrological@labsid.eng.br"
+__copyright__ = 'Copyright 2021, LabSid PHA EPUSP'
+__license__ = "GPL"
 __date__ = '2021-05-19'
-__copyright__ = 'Copyright 2021, LabSid'
+__version__ = "0.1.0"
 
 import os
 
