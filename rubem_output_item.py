b07a78974e6e088b376b7465ecedb08b6503d40b
Gabriel Soares <70075435+soaressgabriel@users.noreply.github.com>
2021-08-24 20:25:10 -0300

    Add data model item to list files in a tree view

---
+++ rubem_output_item.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+# RUBEM Hydrological is a QGIS plugin that assists in setup the RUBEM model:
+# Copyright (C) 2021 LabSid PHA EPUSP
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Contact: rubem.hydrological@labsid.eng.br
+
+"""RUBEM Hydrological plugin tree view data model code."""
+
+__author__ = "LabSid PHA EPUSP"
+__email__ = "rubem.hydrological@labsid.eng.br"
+__copyright__ = "Copyright 2021, LabSid PHA EPUSP"
+__license__ = "GPL"
+__date__ = "2021-05-19"
+__version__ = "1.3.2"
+
+
+from os.path import split
+
+try:
+    from qgis.PyQt.QtGui import QStandardItem
+except ImportError:
+    from PyQt5.QtGui import QStandardItem
+
+# TODO: Add docstring information and comments
+class StandardItem(QStandardItem):
+    def __init__(self, txt="", isPath=False):
+        super().__init__()
+
+        self.setEditable(False)
+
+        if isPath:
+            _, self.title = split(txt)
+            self.setText(self.title)
+        else:
+            self.setText(txt)
